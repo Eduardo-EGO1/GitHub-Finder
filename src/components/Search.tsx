45efb0774090d195dad8c481462e304a3ab54be4
Eduardo-EGO1 <eduardogomes.job@outlook.com>
2025-09-26 11:05:06 -0300

    git commit -m "feat: new user component"

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,19 +4,30 @@ type SearchProps = {
 
 import { useState } from "react";
 
+import type {KeyboardEvent} from "react"
+
 import { BsSearch } from "react-icons/bs";
 
 import classes from "./Search.module.css";
 
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState("");
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            loadUser(userName);
+        }
+    }
     return (
         <div className={classes.search}>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
             <div className={classes.search_container}>
-                <input type = "text" placeholder = "Digite o nome do usuário:" onChange={(e) => setUserName(e.target.value)}/>
-                <button onClick = {() => loadUser(userName)}>
+                <input type = "text" placeholder = "Digite o nome do usuário:"
+                 onChange={(e) => setUserName(e.target.value)}
+                 onKeyDown={handleKeyDown}/>
+                
+                <button onClick = {() => loadUser(userName)} >
                     <BsSearch />
                 </button>
             </div>
